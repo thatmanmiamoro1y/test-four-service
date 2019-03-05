8cc9105f295ecd33cd516fefc0106e43903b637c
ThatMan <miamoro1y@163.com>
2019-03-05 23:26:07 +0800

    Initialization

---
+++ src/main/java/com/thatman/testservice/client/TestServiceOneClient.java
@@ -0,0 +1,16 @@
+package com.thatman.testservice.client;
+
+
+import com.thatman.testservice.Entity.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Component
+@FeignClient(name = "test-one-service",fallback = TestServiceOneClientFallback.class)
+public interface TestServiceOneClient {
+
+    @GetMapping(value ="test/getUser")
+    User getUser();
+
+}
--- src/main/java/com/thatman/testservice/service/impl/TestServiceImpl.java
+++ src/main/java/com/thatman/testservice/service/impl/TestServiceImpl.java
@@ -1,6 +1,7 @@
 package com.thatman.testservice.service.impl;
 
 import com.thatman.testservice.Entity.User;
+import com.thatman.testservice.client.TestServiceOneClient;
 import com.thatman.testservice.client.TestServiceTwoClient;
 import com.thatman.testservice.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,9 @@ public class TestServiceImpl implements TestService {
 
     @Autowired
     TestServiceTwoClient testServiceTwoClient;
+
+    @Autowired
+    TestServiceOneClient testServiceOneClient;
 
 
     @Override
